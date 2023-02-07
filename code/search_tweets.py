2c224b9b8cf2e3360d99453413d3e5e942b921b5
Daniil Shapovalov <yatraik03@gmail.com>
2023-02-07 15:42:12 +0300

    add lowercase conversion in check tweets text

--- code/search_tweets.py
+++ code/search_tweets.py
@@ -2,7 +2,7 @@ import datetime as dt
 
 import tweepy
 
-from services import fill_list_with_tweet_data, is_part_in_list
+from services import fill_list_with_tweet_data
 
 
 def global_search_tweets(api: tweepy.API, search_query: str, time_from_which_start_search: dt.datetime
@@ -32,9 +32,9 @@ def search_tweets_in_profiles(api: tweepy.API, search_query: str, time_from_whic
         tweets = api.user_timeline(screen_name=profile, tweet_mode="extended")
         tweets_list_from_json = [tweet for tweet in tweets if tweet.created_at > time_from_which_start_search]
         for tweet in tweets_list_from_json:
-            if is_part_in_list(tweet.full_text, search_query.split()) and tweet.in_reply_to_status_id is None:
+            if all(word.lower() in tweet.full_text.lower() for word in search_query.split()) and \
+                    tweet.in_reply_to_status_id is None:
                 fill_list_with_tweet_data(all_tweets_needed, tweet)
     if not all_tweets_needed:
         return "2 - Новых твитов за последние 3 часа не обнаружено 😪"
     return all_tweets_needed
-
